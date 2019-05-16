f522fe20f693a38ac9415ffb38b9a5b770566bdc
ionene <ione8ibanez@gmail.com>
2019-05-16 13:09:42 +0200

    subido

---
+++ CalcularPeriodo/src/TestCalculadorea.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+import org.junit.jupiter.api.Test;
+
+class TestCalculadorea {
+
+	@Test
+	void test() {
+		
+		
+	}
+
+}
